4963b15b7e9f70c9626172967a6977e8fc585690
Wolfram Kraus <wolfram.kraus@fen-net.de>
2022-04-01 17:35:07 +0200

    display simple text in status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,30 @@
 import * as vscode from 'vscode';
 
+let fileStatusBarItem: vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
-	
+	// register the command that is invoked on status bar click
+	const fileStatusCommandId = 'filestatus.fileStatus';
+
 	let disposable = vscode.commands.registerCommand('filestatus.fileStatus', () => {
+		console.log('clicked');
 	});
 
 	context.subscriptions.push(disposable);
+
+	// create a new status bar item that we can now manage
+	fileStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	fileStatusBarItem.command = fileStatusCommandId;
+	context.subscriptions.push(fileStatusBarItem);
+
+	// update status bar item once at start
+	updateStatusBarItem();
+
 }
 
-export function deactivate() {}
+function updateStatusBarItem(): void {
+	fileStatusBarItem.text = 'Hello!';
+	fileStatusBarItem.show();
+}
+
+export function deactivate() { }
